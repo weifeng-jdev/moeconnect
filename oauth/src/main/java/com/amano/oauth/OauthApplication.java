198a84b010fa97c76a0574b91e05710d1feba7f1
卫峰 <im.wf@outlook.com>
2022-03-30 14:59:16 +0800

    feat:pom整合mybatis plus, jwt

--- oauth/src/main/java/com/amano/oauth/OauthApplication.java
+++ oauth/src/main/java/com/amano/oauth/OauthApplication.java
@@ -1,12 +1,14 @@
 package com.amano.oauth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@MapperScan(basePackages = "com.amano.oauth.dao")
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class OauthApplication {
 
     public static void main(String[] args) {
@@ -14,3 +16,4 @@ public class OauthApplication {
     }
 
 }
+
--- oauth/src/main/java/com/amano/oauth/config/DataSourceConfiguration.java
+++ oauth/src/main/java/com/amano/oauth/config/DataSourceConfiguration.java
@@ -2,7 +2,6 @@ package com.amano.oauth.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -20,23 +19,6 @@ import javax.sql.DataSource;
 @Configuration
 @Slf4j
 public class DataSourceConfiguration {
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.datasource.max-active:10}")
-    private Integer maxActive;
-    @Value("${spring.datasource.initial-size:5}")
-    private int initialSize;
-    @Value("${spring.datasource.min-idle:5}")
-    private int minIdle;
-    @Value("${spring.datasource.max-wait:10000}")
-    private int maxWait;
-
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource druidDataSource() {
