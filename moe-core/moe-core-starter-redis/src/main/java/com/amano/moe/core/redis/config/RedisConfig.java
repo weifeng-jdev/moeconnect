cd95810b2e4ec8b5ed5f352bc848f410408378d4
卫峰 <im.wf@outlook.com>
2022-04-13 16:03:28 +0800

    feat: 减少重复依赖

--- moe-core/moe-core-starter-redis/src/main/java/com/amano/moe/core/redis/config/RedisConfig.java
+++ moe-core/moe-core-starter-redis/src/main/java/com/amano/moe/core/redis/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.amano.moe.core.redis.config;
 
 import com.amano.moe.core.redis.util.FastJsonRedisSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
